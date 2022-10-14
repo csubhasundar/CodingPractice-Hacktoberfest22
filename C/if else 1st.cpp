a767b739b6f1b03b1bc8f1f430199b1fd3f9e328
Subha Sundar Chakraborty <72214531+csubhasundar@users.noreply.github.com>
2022-10-14 09:59:29 +0530

    Add files via upload

---
+++ C/if else 1st.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	printf("Enter the value of a ");
+	scanf("%d",&a);
+	if(a>40)
+  {
+	
+	printf("pass");
+  } 
+	else
+	{
+		printf("Back ");
+	}
+	return 0;
+}
