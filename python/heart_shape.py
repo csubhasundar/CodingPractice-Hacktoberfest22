bffb123276558c14be2fb2ef5514ccb4ef959d80
Sanket Banerjee <99745967+Sanket2004@users.noreply.github.com>
2022-10-27 17:55:56 +0530

    Create heart_shape.py
    heart shape using python turtle

---
+++ python/heart_shape.py
@@ -0,0 +1,91 @@
+import turtle 
+
+from turtle import *
+
+
+
+wn = Screen()
+
+wn.bgcolor('ivory')
+
+
+
+t = turtle. Turtle()
+
+t.pencolor('red')
+
+
+
+def curve():
+
+       for i in range (200):
+
+             t.rt(1)
+
+             t.fd(1)
+
+       
+
+def heart():
+
+       t.fillcolor('red')
+
+       t.begin_fill()
+
+       t.lt (140)
+
+       t.fd (113)
+
+       curve () 
+
+       t.lt (120)
+
+       curve() 
+
+       t.fd(112)
+
+       t.end_fill()
+
+
+
+heart()
+
+t.ht()
+
+
+
+def write(message,pos):
+
+       x,y=pos
+
+       t.penup()
+
+       t.goto(x,y)
+
+       t.color('white')
+
+       style=("Stencil Std", 18, "italic")
+
+       t.write(message, font=style)
+
+
+
+write('I',(-68,95))
+
+write('L',(-55,95))
+
+write('0',(-42,95))
+
+write('V',(-30,95))
+
+write('E',(-14,95))
+
+write('Y', (10,95))
+
+write('0', (26,95))
+
+write('U', (45,95))
+
+
+
+wn.mainloop()
